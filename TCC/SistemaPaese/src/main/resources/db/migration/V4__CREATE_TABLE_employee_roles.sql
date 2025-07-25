b41a77ec1c408224e6e0d369019c04e3ef0c95d4
Luiz Fernando Venceslau <venceslau.luizfernando@oulook.com>
2025-07-24 21:20:49 -0300

    Corrigindo atributios da Migration

--- TCC/SistemaPaese/src/main/resources/db/migration/V4__CREATE_TABLE_employee_roles.sql
+++ TCC/SistemaPaese/src/main/resources/db/migration/V4__CREATE_TABLE_employee_roles.sql
@@ -2,14 +2,14 @@ CREATE TABLE employee_roles (
 employee_id INTEGER,
 role_id INTEGER REFERENCES role(id),
 active BOOLEAN DEFAULT TRUE NOT NULL,
-creation_date TIMESTAMP NOT NULL,
-created_by_id INTEGER NOT NULL,
-deactivated_by_id INTEGER,
-deactivation_date DATE,
+granted_at TIMESTAMP NOT NULL,
+granted_by INTEGER NOT NULL,
+revoked_by INTEGER,
+revoked_at TIMESTAMP,
 CONSTRAINT pk_employee_roles PRIMARY KEY(employee_id, role_id),
-CONSTRAINT fk_employee_roles_employee_id FOREIGN KEY(employee_id) RERERENCES employees(id),
-CONSTRAINT fk_employee_roles_role_id FOREIGN KEY(role_id) RERERENCES roles(id),
-CONSTRAINT fk_employee_roles_created_by FOREIGN KEY(created_by_id) RERERENCES employees(id),
-CONSTRAINT fk_employee_roles_deactivated_by FOREIGN KEY(deactivated_by_id) RERERENCES employees(id)
+CONSTRAINT fk_employee_roles_employee_id FOREIGN KEY(employee_id) REFERENCES employees(id),
+CONSTRAINT fk_employee_roles_role_id FOREIGN KEY(role_id) REFERENCES roles(id),
+CONSTRAINT fk_granted_by FOREIGN KEY(granted_by) REFERENCES employees(id),
+CONSTRAINT fk_revoked_by FOREIGN KEY(revoked_by) REFERENCES employees(id)
 
 );
