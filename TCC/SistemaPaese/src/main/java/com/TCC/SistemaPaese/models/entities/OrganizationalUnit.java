eb8158cbab440df210793448212f418f4ba361ee
Luiz Fernando Venceslau <venceslau.luizfernando@oulook.com>
2025-07-24 21:33:31 -0300

    Criação da classe OrganizationalUnit.java

---
+++ TCC/SistemaPaese/src/main/java/com/TCC/SistemaPaese/models/entities/OrganizationalUnit.java
@@ -0,0 +1,35 @@
+package com.TCC.SistemaPaese.models.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "organizational_units")
+public class OrganizationalUnit extends BasicAttributes{
+
+    @Size(max = 255)
+    @Column(length = 255, name = "name", nullable = false)
+    private String name;
+    @Size(max = 70)
+    @Column(length = 70, name = "type", nullable = false)
+    private String type;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id", nullable = false)
+    private OrganizationalUnit parent;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by_id", nullable = false)
+    private Employee createdBy;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "deactivated_by_id")
+    private Employee deactivatedBy;
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private List<OrganizationalUnit> children = new ArrayList<>();
+
+    public OrganizationalUnit(){}
+
+
+
+}
