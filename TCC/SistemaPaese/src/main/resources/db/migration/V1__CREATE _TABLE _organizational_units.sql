b54a5513b241ed46fad71f5c88d0593b40e33cca
Luiz Fernando Venceslau <venceslau.luizfernando@oulook.com>
2025-07-26 14:57:55 -0300

    Removendo as Constraints de referencia a tebal employess

--- TCC/SistemaPaese/src/main/resources/db/migration/V1__CREATE _TABLE _organizational_units.sql
+++ TCC/SistemaPaese/src/main/resources/db/migration/V1__CREATE _TABLE _organizational_units.sql
@@ -9,12 +9,10 @@ created_by_id INTEGER NOT NULL,
 deactivation_date TIMESTAMP,
 deactivated_by_id INTEGER,
 CONSTRAINT pk_organizational_unit PRIMARY KEY(id),
-CONSTRAINT fk_organizational_unit_created_by FOREIGN KEY(created_by_id) REFERENCES employees(id),
-CONSTRAINT fk_organizational_unit_deactivated_by FOREIGN KEY(deactivated_by_id) REFERENCES employees(id),
-CONSTRAINT fk_parent FOREIGN KEY(parent_id) REFERENCES organizational_unit(id)
+CONSTRAINT fk_parent FOREIGN KEY(parent_id) REFERENCES organizational_units(id)
     ON DELETE SET NULL
     ON UPDATE CASCADE
 
 );
 
-CREATE INDEX idx_organizational_unit_parent_id ON organizational_unit(parent_id);
+CREATE INDEX idx_organizational_unit_parent_id ON organizational_units(parent_id);
