df5d797b5160eca3501d739e1c98e7fa65f5d441
Luiz Fernando Venceslau <venceslau.luizfernando@oulook.com>
2025-07-26 15:08:24 -0300

    Corrigindo erros de sintaxe

--- TCC/SistemaPaese/src/main/resources/db/migration/V6__CREATE_TABLE_records.sql
+++ TCC/SistemaPaese/src/main/resources/db/migration/V6__CREATE_TABLE_records.sql
@@ -15,6 +15,6 @@ CONSTRAINT pk_records PRIMARY KEY(id),
 CONSTRAINT fk_records_incident FOREIGN KEY(incident_id) REFERENCES incidents(id),
 CONSTRAINT fk_records_created_by FOREIGN KEY(created_by_id) REFERENCES employees(id),
 CONSTRAINT fk_records_deactivated_by FOREIGN KEY(deactivated_by_id) REFERENCES employees(id),
-CONSTRAINT fk_records_updated_by FOREIGN KEY(update_by_id) REFERENCES employees(id)
+CONSTRAINT fk_records_updated_by FOREIGN KEY(updated_by_id) REFERENCES employees(id)
 
 );
